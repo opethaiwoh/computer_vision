dd6962c0024d88a5d4bdb8403f471270a0f8877e
Adeniran Opeyemi Taiwo <60190827+opethaiwoh@users.noreply.github.com>
2023-04-16 12:41:49 -0400

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'walletwi_walletwi_wizardb_users');
+define('DB_PASSWORD', 'walletwi_wizardb_users');
+define('DB_NAME', 'walletwi_wizardb');
+ 
